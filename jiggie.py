86bf37bd70d43d7146c4fe0b041b4d41aa5420ba
Derek Mayer <deverex1317@gmail.com>
2014-12-04 11:46:52 -0600

    Started laying the groundwork for the config and starting the actual program.

--- jiggie.py
+++ jiggie.py
@@ -1,4 +1,18 @@
+import os, sys, logging, argparse, json
+
+from helper import config
+
+parser = argparse.ArgumentParser(description='Jiggie')
+parser.add_argument('--debug', action='store_true', help='Show debug messages')
+args = parser.parse_args()
+
+
 if __name__ == '__main__':
+    print("~~~~~~~~~~~~~~~~~~~~~~")
+    print(" Jiggie Voice Commands ")
+    print(" Made By: Derek Mayer")
+    print("~~~~~~~~~~~~~~~~~~~~~~")
 
-
+    logging.basicConfig()
+    logger = logging.getLogger()
 
